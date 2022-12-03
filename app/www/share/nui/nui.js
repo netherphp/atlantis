be2e5dc1e47c407384f6a49a5d136006d65fe4cb
bobmagicii <bob@majdak.net>
2022-12-03 09:30:21 +0000

    thusfar

--- app/www/share/nui/nui.js
+++
@@ -1,9 +0,0 @@
-import { JsonRequest, JsonResult } from '/share/nui/api/json.js';
-import FormUtil from '/share/nui/util/form.js';
-
-const NUI = {
-	Request: JsonRequest,
-	Form: FormUtil
-};
-
-export default NUI;
