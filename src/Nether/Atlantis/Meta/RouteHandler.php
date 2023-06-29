df86c64dfaade0ba57c612aa1e53ad30ab781404
bob <bob@tech.live>
2023-06-29 01:00:48 +0000

    thusfar

--- src/Nether/Atlantis/Meta/RouteHandler.php
+++ src/Nether/Atlantis/Meta/RouteHandler.php
@@ -1,12 +1,21 @@
 <?php
 
 namespace Nether\Atlantis\Meta;
-use Nether;
+
+use Nether\Atlantis;
+use Nether\Avenue;
+use Nether\Common;
 
 use Attribute;
 
 #[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
 class RouteHandler
-extends Nether\Avenue\Meta\RouteHandler {
+extends Avenue\Meta\RouteHandler {
+
+	public function
+	__Construct(?string $Path=NULL, ?string $Domain=NULL, ?string $Verb='GET', ?string $Sort=NULL) {
+		parent::__Construct($Path, $Domain, $Verb, $Sort);
+		return;
+	}
 
 }
