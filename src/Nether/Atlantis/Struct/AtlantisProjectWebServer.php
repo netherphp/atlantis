2f753e45032d183e6ec3d778eec37e1ef39c24df
bobmagicii <bob@majdak.net>
2023-10-13 21:19:17 +0000

    thusfar

--- src/Nether/Atlantis/Struct/AtlantisProjectWebServer.php
+++ src/Nether/Atlantis/Struct/AtlantisProjectWebServer.php
@@ -69,6 +69,63 @@ implements
 	////////////////////////////////////////////////////////////////
 
 	public function
+	ToApache24(Atlantis\Engine $App):
+	string {
+
+		$Uses = new Common\Datastore;
+		$Output = NULL;
+
+		$TemplateFile = $App->FromProjectRoot(Common\Filesystem\Util::Pathify(
+			'vendor', 'netherphp', 'atlantis',
+			'templates', 'apache24.txt'
+		));
+
+		////////
+
+		if(!file_exists($TemplateFile))
+		throw new Common\Error\FileNotFound($TemplateFile);
+
+		($this->Domains)
+		->Each(function(string $Line) use($App, $Uses) {
+			$Bits = explode(' ', $Line);
+			$Bit = NULL;
+			$Dom = current($Bits);
+
+			// Use $Macro $Domain $SSLDomain $WebRoot
+
+			foreach($Bits as $Bit)
+			$Uses->Push(sprintf(
+				'Use %s %s %s %s',
+				($this->HTTPS ? 'HTTPS' : 'HTTP'),
+				$Bit,
+				$Dom,
+				$App->GetWebRoot()
+			));
+
+			return;
+		});
+
+		////////
+
+		$Output = file_get_contents($TemplateFile);
+
+		Common\Datastore::FromArray([
+			'DOMAINS' => $Uses->Join(PHP_EOL)
+		])
+		->Each(function(string $V, string $K) use(&$Output) {
+			$Output = str_replace("{%{$K}%}", $V, $Output);
+			return;
+		});
+
+		////////
+
+		return $Output;
+	}
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	public function
 	ToArray():
 	array {
 
