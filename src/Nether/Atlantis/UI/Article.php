b75a35cd1a40f97addecacbd172335a199886385
bobmagicii <bob@majdak.net>
2023-10-14 00:39:22 +0000

    thusfar

---
+++ src/Nether/Atlantis/UI/Article.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Nether\Atlantis\UI;
+
+use Nether\Surface;
+
+class Article
+extends Surface\Element {
+
+	public string
+	$Area = 'elements/article/main';
+
+	public ?string
+	$Class = NULL;
+
+	public ?string
+	$Title = NULL;
+
+	public ?string
+	$TitleClass = NULL;
+
+	public ?string
+	$Date = NULL;
+
+	public ?string
+	$DateClass = NULL;
+
+	public ?string
+	$Section = NULL;
+
+	public ?string
+	$SectionClass = NULL;
+
+	public ?string
+	$Content = NULL;
+
+	public ?string
+	$ContentClass = NULL;
+
+}
