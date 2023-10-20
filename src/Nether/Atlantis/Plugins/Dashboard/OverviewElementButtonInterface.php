15795562d4af4e9cacdc94d9cd1b78d4b0421ae1
bob <bob@tech.live>
2023-10-20 22:56:57 +0000

    thusfar

--- src/Nether/Atlantis/Plugins/Dashboard/OverviewElementButtonInterface.php
+++ src/Nether/Atlantis/Plugins/Dashboard/OverviewElementButtonInterface.php
@@ -2,7 +2,16 @@
 
 namespace Nether\Atlantis\Plugins\Dashboard;
 
+use Nether\Atlantis;
+
 interface OverviewElementButtonInterface {
+
+	public function
+	__Construct(Atlantis\Engine $App);
+
+	public function
+	GetShow():
+	bool;
 
 	public function
 	GetName():
