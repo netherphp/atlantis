b1c5f75fccbf90f8916cb37a39cab9c44d0c29c9
bob <bob@tech.live>
2023-10-20 22:44:40 +0000

    thusfar

---
+++ src/Nether/Atlantis/Plugins/Dashboard/OverviewElementButtonInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Nether\Atlantis\Plugins\Dashboard;
+
+interface OverviewElementButtonInterface {
+
+	public function
+	GetName():
+	?string;
+
+	public function
+	GetIcon():
+	?string;
+
+	public function
+	GetURL():
+	?string;
+
+};
