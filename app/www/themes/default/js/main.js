b3c1517b40b00e4a3ebb93cc55baf9b547a48aa5
bobmagicii <bob@majdak.net>
2023-10-24 01:08:17 +0000

    thusfar

--- app/www/themes/default/js/main.js
+++ app/www/themes/default/js/main.js
@@ -2,6 +2,9 @@ import NUIUtil from '/share/nui/util.js';
 
 function initMenu() {
 
+	// @deprecated 2023-10-23
+	// new theme uses SiteMenu element.
+
 	let menu = jQuery('.PageHeader');
 	let menutoggle = jQuery('.PageHeaderMenuToggle');
 
@@ -60,11 +63,62 @@ function initColourModer() {
 	return;
 };
 
+function initColourSwitch() {
+
+	jQuery('input[data-nui-theme-switch]')
+	.on('change', function() {
+
+		let that = jQuery(this);
+		let vals = that.attr('data-nui-theme-switch').split('/');
+		let on = that.is(':checked');
+		let theme = vals[on ? 1 : 0];
+
+		document.cookie = `theme=${theme};path=/;max-age=31536000`;
+		document.documentElement.dataset.bsTheme = theme;
+
+		pingColourSwitch();
+
+		return;
+	});
+
+	pingColourSwitch();
+
+	return;
+};
+
+function pingColourSwitch() {
+
+	jQuery('input[data-nui-theme-switch]')
+	.each(function() {
+
+		let that = jQuery(this);
+		let vals = that.attr('data-nui-theme-switch').split('/');
+
+		if(document.cookie.indexOf(`theme=${vals[0]}`) >= 0) {
+			console.log(`current theme: dark ${vals[0]}`);
+			that.removeAttr('checked');
+			return;
+		}
+
+		if(document.cookie.indexOf(`theme=${vals[1]}`) >= 0) {
+			console.log(`current theme: light ${vals[1]}`);
+			that.attr('checked', 'checked');
+			return;
+		}
+
+		return;
+	});
+
+	return;
+};
+
 jQuery(function(){
 
-	initColourModer();
-	initMenu();
+	initColourSwitch();
 	initCopyLinks();
+
+	initColourModer(); // @deprecated 2023-10-23.
+	initMenu();        // @deprecated 2023-10-23.
 
 	return;
 });
