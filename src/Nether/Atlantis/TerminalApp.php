a073672d3218c4207a6137373c063bcc7cb8dea3
bobmagicii <bob@majdak.net>
2023-11-15 21:25:25 +0000

    thusfar

---
+++ src/Nether/Atlantis/TerminalApp.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Nether\Atlantis;
+
+use Nether\Console;
+
+class TerminalApp
+extends Console\Client {
+
+	protected Engine
+	$App;
+
+	protected function
+	OnReady():
+	void {
+
+		// one interesting thing of note here is that when given the
+		// foreign path there is no version checking currently and that
+		// code is going to ask for classes that will be served from the
+		// local autoloader.
+
+		$ProjectRoot = $this->GetOption('atlantis');
+
+		////////
+
+		if(is_string($ProjectRoot))
+		$ProjectRoot = realpath($ProjectRoot);
+
+		if(!is_string($ProjectRoot))
+		$ProjectRoot = Util::GetBinProjectDirectory($this->File);
+
+		////////
+
+		$this->App = new Engine($ProjectRoot);
+
+		return;
+	}
+
+};
