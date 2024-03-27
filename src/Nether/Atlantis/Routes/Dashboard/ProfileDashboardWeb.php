57791c90a8e6663249b862bb87e718d486eb5e79
bob <bob@tech.live>
2024-03-27 00:20:00 +0000

    thusfar

--- src/Nether/Atlantis/Routes/Dashboard/ProfileDashboardWeb.php
+++ src/Nether/Atlantis/Routes/Dashboard/ProfileDashboardWeb.php
@@ -18,27 +18,47 @@ extends Atlantis\ProtectedWeb {
 		($this->Data)
 		->Q(Common\Filters\Text::TrimmedNullable(...))
 		->Untagged(Common\Filters\Numbers::BoolNullable(...))
-		->Page(Common\Filters\Numbers::Page(...));
+		->Page(Common\Filters\Numbers::Page(...))
+		->SiteTag(Common\Filters\Text::TrimmedNullable(...))
+		->Sort(Common\Filters\Misc::OneOfTheseFirst(...), [ 'title-az', 'title-za' ]);
 
 		$Trail = new Common\Datastore([
 			'Profiles' => '/dashboard/profiles'
 		]);
 
+		////////
+
+		$SiteTags = Atlantis\Tag\Entity::Find([
+			'Type' => 'site'
+		]);
+
+		$SiteTag = NULL;
+
+		if($this->Data->SiteTag)
+		$SiteTag = Atlantis\Tag\Entity::GetByField('Alias', $this->Data->SiteTag);
+
 		$Filters = [
 			'Enabled'  => FALSE,
+			'UseSiteTags' => FALSE,
 			'Search'   => $this->Data->Q,
 			'Untagged' => $this->Data->Untagged,
 			'Page'     => $this->Data->Page,
-			'Sort'     => 'name-az',
+			'Sort'     => $this->Data->Sort,
 			'Limit'    => 20
 		];
 
+		if($SiteTag)
+		$Filters['TagID'] = $SiteTag->ID;
+
 		$Profiles = Atlantis\Profile\Entity::Find($Filters);
+		$Profiles->Each(fn(Atlantis\Profile\Entity $P)=> $P->GetTags());
 
 		$Searched = (FALSE
 			|| $this->Data->Q
 			|| $this->Data->Untagged
 		);
+
+
 
 		////////
 
@@ -46,6 +66,8 @@ extends Atlantis\ProtectedWeb {
 		->Set('Page.Title', 'Manage Profiles - Dashboard')
 		->Wrap('atlantis/dashboard/profile/index', [
 			'Trail'    => $Trail,
+			'SiteTags' => $SiteTags,
+			'SiteTag'  => $SiteTag,
 			'Filters'  => $Filters,
 			'Searched' => $Searched,
 			'Profiles' => $Profiles
