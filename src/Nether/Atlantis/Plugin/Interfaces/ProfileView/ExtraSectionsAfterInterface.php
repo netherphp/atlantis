99cb3a91c43600c45936cd6c1a88ce43034ab81a
bob <bob@tech.live>
2024-05-29 00:05:00 +0000

    thusfar

--- src/Nether/Atlantis/Plugin/Interfaces/ProfileView/ExtraSectionsAfterInterface.php
+++ src/Nether/Atlantis/Plugin/Interfaces/ProfileView/ExtraSectionsAfterInterface.php
@@ -7,6 +7,10 @@ use Nether\Common;
 
 interface ExtraSectionsAfterInterface {
 
+	// fill the datastore with things that can be printed, or if given a
+	// callable it will call that with the curent Surface Area $__SCOPE array
+	// passed in as the first argument.
+
 	public function
 	GetExtraSectionsAfter(Atlantis\Profile\Entity $Profile):
 	Common\Datastore;
--- src/Nether/Atlantis/Plugin/Interfaces/ProfileView/ExtraSectionsBeforeInterface.php
+++ src/Nether/Atlantis/Plugin/Interfaces/ProfileView/ExtraSectionsBeforeInterface.php
@@ -7,6 +7,10 @@ use Nether\Common;
 
 interface ExtraSectionsBeforeInterface {
 
+	// fill the datastore with things that can be printed, or if given a
+	// callable it will call that with the curent Surface Area $__SCOPE array
+	// passed in as the first argument.
+
 	public function
 	GetExtraSectionsBefore(Atlantis\Profile\Entity $Profile):
 	Common\Datastore;
