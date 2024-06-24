878f3216b72b3a57da9b82427f6ac7761a49a94f
bobmagicii <bob@majdak.net>
2024-06-24 05:43:25 +0000

    thusfar

---
+++ app/www/share/atlantis/lib/editorjs/tools/atl-hr.js
@@ -0,0 +1,128 @@
+class PluginHR {
+/*//
+@date 2020-10-11
+provides a custom block plugin for editor.js for writing blocks of code into
+a piece of content using codemirror as the code syntax magic thing.
+//*/
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	static get isInline() {
+		return false;
+	};
+
+	static get toolbox() {
+		return {
+			title: 'HR Break.',
+			icon: '<i class="mdi mdi-format-page-break"></i>'
+		};
+	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	constructor(data) {
+
+		this.element = null;
+		this.data = data;
+
+		if(typeof this.data.mode === 'undefined')
+		this.data.mode = 'line';
+
+		return;
+	};
+
+	render() {
+
+		this.element = jQuery('<hr class="atl-editorjs-hr" />');
+		this.btnLine = null;
+		this.btnEmpty = null;
+
+		this.element.addClass(this.data.mode);
+
+		return this.element.get()[0];
+	};
+
+	renderSettings() {
+
+		this.settings = jQuery('<div />');
+
+		(this.settings)
+		.append(
+			this.btnLine = jQuery('<div />')
+			.addClass('ce-popover-item')
+			.addClass(this.data.mode === 'line' ? 'ce-popover-item--active' : '')
+			.append(
+				jQuery('<div />')
+				.addClass('ce-popover-item__icon')
+				.html('<i class="mdi mdi-minus"></i>')
+			)
+			.append(
+				jQuery('<div />')
+				.addClass('ce-popover-item__title')
+				.text('Line Break')
+			)
+			.on('click', this.onSetLineBreak.bind(this))
+		)
+		.append(
+			this.btnEmpty = jQuery('<div />')
+			.addClass('ce-popover-item')
+			.addClass(this.data.mode === 'empty' ? 'ce-popover-item--active' : '')
+			.append(
+				jQuery('<div />')
+				.addClass('ce-popover-item__icon')
+				.html('<i class="mdi mdi-drag-horizontal"></i>')
+			)
+			.append(
+				jQuery('<div />')
+				.addClass('ce-popover-item__title')
+				.text('Space Break')
+			)
+			.on('click', this.onSetSpaceBreak.bind(this))
+		);
+
+		return this.settings.get()[0];
+	};
+
+	save(data) {
+
+		return {
+			mode: this.data.mode
+		};
+	};
+
+	onSetLineBreak() {
+
+		this.data.mode = 'line';
+
+		(this.element)
+		.addClass('line')
+		.removeClass('empty');
+
+		this.btnLine.addClass('ce-popover-item--active');
+		this.btnEmpty.removeClass('ce-popover-item--active');
+
+		return;
+	};
+
+	onSetSpaceBreak() {
+
+		this.data.mode = 'empty';
+
+		this.element
+		.addClass('empty')
+		.removeClass('line');
+
+		this.btnEmpty.addClass('ce-popover-item--active');
+		this.btnLine.removeClass('ce-popover-item--active');
+
+		return;
+	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+};
+
+export default PluginHR;
---
+++ app/www/share/atlantis/lib/editorjs/tools/atl-tt.js
@@ -0,0 +1,48 @@
+class PluginHR {
+/*//
+@date 2020-10-11
+provides a custom block plugin for editor.js for writing blocks of code into
+a piece of content using codemirror as the code syntax magic thing.
+//*/
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	static get isInline() {
+		return true;
+	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	constructor() {
+
+		return;
+	};
+
+	render() {
+
+		this.btn = jQuery('<button />');
+		this.btn.addClass('ce-inline-tool');
+		this.btn.attr('type', 'button');
+		this.btn.html('<i class="mdi mdi-format-page-break"></i>');
+
+		return this.btn.get()[0];
+	};
+
+	surround(range) {
+
+		return;
+	};
+
+	checkState(selection) {
+
+		return;
+	};
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+};
+
+export default PluginHR;
