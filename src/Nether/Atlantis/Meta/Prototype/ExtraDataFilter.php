4dad7749d24aebf2e1ae2d6a1bfc448dba91d3cd
bob <bob@tech.live>
2024-07-10 02:35:31 +0000

    thusfar

--- src/Nether/Atlantis/Meta/Prototype/ExtraDataFilter.php
+++ src/Nether/Atlantis/Meta/Prototype/ExtraDataFilter.php
@@ -11,6 +11,13 @@ use Attribute;
 #[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
 class ExtraDataFilter {
 
+	// this attribute's main purpose is to define extra data key-value
+	// filterings so that prototype objects can continue to use the ExtraData
+	// willy nilly while preventing garbage input and allowing for
+	// reinforcement of the data that came in into its proper hard type
+	// when needed. this gives ExtraData simliar sanitisation features as
+	// class properties with the PropertyFilter attribute.
+
 	public string
 	$Key;
 
