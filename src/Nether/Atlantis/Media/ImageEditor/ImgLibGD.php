99578d0ae7a73f720254e836e84ff1e6cce887cd
bobmagicii <bob@majdak.net>
2024-12-14 21:16:42 +0000

    hadnt expected that to crater my unit tests since nothing calls it yet

--- src/Nether/Atlantis/Media/ImageEditor/ImgLibGD.php
+++ src/Nether/Atlantis/Media/ImageEditor/ImgLibGD.php
@@ -9,6 +9,52 @@ namespace Nether\Atlantis\Media\ImageEditor;
 class ImgLibGD
 extends ImgLib {
 
-	// ...
+	public function
+	Open(string $Filename):
+	static {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return $this;
+	}
+
+	public function
+	Close():
+	static {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return $this;
+	}
+
+	public function
+	Save(string $Filename):
+	static {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return $this;
+	}
+
+	////////////////////////////////
+	////////////////////////////////
+
+	public function
+	IsOpen():
+	bool {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return FALSE;
+	}
+
+	////////////////////////////////
+	////////////////////////////////
+
+	public function
+	Fit(int $W, int $H):
+	static {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return $this;
+	}
+
+	public function
+	RemoveMetadata():
+	static {
+		throw new \Nether\Atlantis\Error\MethodUnimplemented(__METHOD__);
+		return $this;
+	}
 
 };
