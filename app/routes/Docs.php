3417633217153a6a9ecd4f4e720dcd23c38ca3a0
bob <bob@tech.live>
2025-05-13 15:29:34 -0500

    thusfar

--- app/routes/Docs.php
+++ app/routes/Docs.php
@@ -25,6 +25,9 @@ extends Atlantis\PublicWeb {
 		return;
 	}
 
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
 	#[Avenue\Meta\RouteHandler('/docs/::Path::')]
 	#[Avenue\Meta\ConfirmWillAnswerRequest]
 	#[Avenue\Meta\ExtraDataArgs]
@@ -63,127 +66,31 @@ extends Atlantis\PublicWeb {
 		return Avenue\Response::CodeOK;
 	}
 
-	/*
-	#[Avenue\Meta\RouteHandler('/docs')]
-	public function
-	Index():
-	void {
-
-		$PTitle = 'Framework Overview';
-		$Pathbar = Atlantis\UI\Pathbar::FromSurface($this->Surface);
-
-		////////
-
-		($Pathbar->Items)
-		->Push(Atlantis\Struct\Item::New(
-			Title: 'Docs', URL: '/docs', Classes: [ 'tag' ]
-		));
-
-		////////
-
-		($this->Surface)
-		->Set('Page.Title', $PTitle)
-		->Area(
-			'sensei/docs/__header',
-			[ 'Section'=> $Pathbar, 'Title'=> $PTitle ]
-		)
-		->Area('sensei/docs/index');
-
-		return;
-	}
-
 	////////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////////
 
-	#[Avenue\Meta\RouteHandler('/docs/:Sect:')]
-	#[Avenue\Meta\RouteHandler('/docs/:Sect:/:Page:')]
-	#[Avenue\Meta\ConfirmWillAnswerRequest]
-	#[Avenue\Meta\ExtraDataArgs]
 	public function
-	SectionNew(?string $Sect=NULL, ?string $Page=NULL, ?string $Area=NULL):
-	void {
+	FetchHeading(string $Title, iterable $Trail=[]):
+	string {
 
-		$Pathbar = Atlantis\UI\Pathbar::FromSurface($this->Surface);
-
-		$Content = $this->Surface->GetArea($Area, [
-			'Section' => $Sect,
-			'Page'    => $Page
+		$Output = $this->Surface->GetArea('sensei/docs/__section', [
+			'Title' => $Title,
+			'Trail' => $Trail
 		]);
 
-		$PTitle = $this->Surface->Get('Page.Title');
-		$PInfo = $this->Surface->Get('Page.Info');
-		$PLink = sprintf('/docs/%s/%s', $Sect, $Page);
+		return $Output;
+	}
 
-		$STitle = $this->Surface->Get('Page.Section.Title');
-		$SInfo = $this->Surface->Get('Page.Section.Info');
-		$SLink = $this->Surface->Get('Page.Section.Link') ?? "/docs/{$Sect}";
+	public function
+	FetchExamplePHP(string $Filename):
+	string {
 
-		////////
-
-		($Pathbar->Items)
-		->Push(Atlantis\Struct\Item::New(
-			Title: 'Docs', URL: '/docs', Classes: [ 'tag' ]
-		));
-
-		if($STitle)
-		$Pathbar->Items->Push(Atlantis\Struct\Item::New(
-			Title: $STitle, URL: $SLink
-		));
-
-		////////
-
-		$Header = $this->Surface->GetArea('sensei/docs/__header', [
-			'Section'  => $Pathbar,
-			'Title'    => $PTitle,
-			'Info'     => $PInfo,
-			'TitleURL' => $PLink
+		$Output = $this->Surface->GetArea('sensei/docs/__example-via-php', [
+			'Filename' => $Filename
 		]);
 
-		////////
-
-		echo $Header;
-		echo $Content;
-
-		return;
+		return $Output;
 	}
 
-	protected function
-	SectionNewWillAnswerRequest(?string $Sect=NULL, ?string $Page=NULL, ?Avenue\Struct\ExtraData $Data=NULL):
-	int {
-
-		$Area = NULL;
-		$Path = NULL;
-
-		////////
-
-		$Area = match(TRUE) {
-			($Sect && $Page)
-			=> sprintf('sensei/docs/%s/%s/index', $Sect, $Page),
-
-			($Sect && !$Page)
-			=> sprintf('sensei/docs/%s/index', $Sect),
-
-			default
-			=> NULL
-		};
-
-		if($Area === NULL)
-		return Avenue\Response::CodeNotFound;
-
-		////////
-
-		$Path = $this->Surface->FindAreaFile($Area);
-
-		if($Path === NULL)
-		return Avenue\Response::CodeNotFound;
-
-		////////
-
-		$Data['Area'] = $Area;
-		$Data['Path'] = $Path;
-
-		return Avenue\Response::CodeOK;
-	}
-	*/
 
 };
