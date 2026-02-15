bbf173265c9547255620addfadd57449ccb4ebce
bobmagicii <bob@pegasusgate.net>
2026-02-15 20:18:25 +0000

    thusfar

--- src/Nether/Atlantis/Util/CertInfo.php
+++ src/Nether/Atlantis/Util/CertInfo.php
@@ -249,6 +249,7 @@ extends Common\Prototype {
 		////////
 
 		curl_setopt($Curl, CURLOPT_URL, sprintf('https://%s', $Domain));
+		curl_setopt($Curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0');
 		curl_setopt($Curl, CURLOPT_SSL_VERIFYPEER, FALSE);
 		curl_setopt($Curl, CURLOPT_CERTINFO, TRUE);
 		curl_setopt($Curl, CURLOPT_RETURNTRANSFER, TRUE);
